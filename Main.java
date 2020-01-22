24bb815721fafa3ed5306202905947ab3a0c7f1b
Paritosh Pradeep <paritosh.9423@gmail.com>
2020-01-22 17:34:28 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,18 @@
+class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+  /*//Kadane's Algo
+  int maxEndingHere = 0 ;
+        int maxSoFar = 0;
+        for(int i=0;i<nums.length;i++){
+            maxEndingHere = maxEndingHere + nums[i];
+            if(maxEndingHere < 0)
+                maxEndingHere = 0;
+            if(maxSoFar<maxEndingHere)
+                maxSoFar = maxEndingHere;
+        }
+        return maxSoFar;
+
+  */
+  }
+}
